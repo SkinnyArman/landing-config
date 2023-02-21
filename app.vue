460e9ac7c8de5c75120eabdad795803b657e0f9e
Arman Amini <armanwithamini@gmail.com>
2023-02-21 11:03:25 +0330

    remove extra keywords

--- app.vue
+++ app.vue
@@ -4,12 +4,12 @@
     <app-footer :class="{ 'mt-45': lgAndUp }"></app-footer>
     <h2>{{ data }}</h2>
     <h1>s</h1>
-    <!-- <region-group></region-group> -->
   </v-app>
 </template>
 
 <script setup>
 import { useDisplay } from "vuetify";
 const { lgAndUp } = useDisplay();
-const { data, pending, error, refresh } = await useFetch('https://api.abrhold.com/products?region_id=DTX&type=VM')
+const { data } = await useFetch('https://api.abrhold.com/products?region_id=DTX&type=VM')
+
 </script>
