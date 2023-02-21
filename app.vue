06d0051af8ec86943bca8202fa4637a86f8b5923
Arman Amini <armanwithamini@gmail.com>
2023-02-21 10:29:25 +0330

    add mock api call

--- app.vue
+++ app.vue
@@ -2,6 +2,8 @@
   <v-app id="app">
     <app-header></app-header>
     <app-footer :class="{ 'mt-45': lgAndUp }"></app-footer>
+    <h2>{{ data }}</h2>
+    <h1>s</h1>
     <!-- <region-group></region-group> -->
   </v-app>
 </template>
@@ -9,4 +11,5 @@
 <script setup>
 import { useDisplay } from "vuetify";
 const { lgAndUp } = useDisplay();
+const { data, pending, error, refresh } = await useFetch('https://api.abrhold.com/products?region_id=DTX&type=VM')
 </script>
