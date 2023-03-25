b92be4d3562053aecfe36c2fbfd61b570e1a6bdf
Arman Amini <armanwithamini@gmail.com>
2023-03-25 12:42:00 +0430

    fix type warning

--- app.vue
+++ app.vue
@@ -2,22 +2,26 @@
   <v-app id="app">
     <app-header></app-header>
     <app-footer :class="{ 'mt-45': lgAndUp }"></app-footer>
-    <region-group :locationList="regionList" :default-location="instance.selectedLocation"></region-group>
+    <region-group
+      :locationList="regionList"
+      :default-location="instance.selectedLocation"
+    ></region-group>
   </v-app>
 </template>
 
 <script setup lang="ts">
 import { useDisplay } from "vuetify";
-import { Region } from './types/region'
-import { Instance } from './types/instance'
+import { Region } from "./types/region";
+import { Instance } from "./types/instance";
 
 const { lgAndUp } = useDisplay();
 
-const regionList = ref<Region[]>([])
+const regionList = ref<Region[]>([]);
 const instance = ref(<Instance>{
-  selectedLocation: { id: "DTX", configId: null, abbr: "dtx" }
-})
-const { data } = await useFetch<Region[]>('https://dev3.cloudzy.com/api/regions?productId=172&slug=home')
-regionList.value = data.value
-
+  selectedLocation: { id: "DTX", configId: null, abbr: "dtx" },
+});
+const { data } = await useFetch<Region[]>(
+  "https://dev3.cloudzy.com/api/regions?productId=172&slug=home"
+);
+regionList.value = data.value as Region[];
 </script>
--- components/RegionGroup/RegionGroup.vue
+++ components/RegionGroup/RegionGroup.vue
@@ -5,33 +5,53 @@
       <span class="subtitle-2 grey--text">(server location)</span>
     </h3>
     <v-row class="mb-4" dense>
-      <v-col v-for="location in locationList" :key="location.id" xs="12" cols="12" sm="6" md="6" lg="4"
-        class="navy--text font-weight-medium">
-        <tag-manager-wrapper :id="location.name" @click="$emit('setLocation', location)">
-          <div class="region-card px-2 py-2 d-flex align-center text--blue-dark font-weight-medium" :id="location.name"
+      <v-col
+        v-for="location in locationList"
+        :key="location.id"
+        xs="12"
+        cols="12"
+        sm="6"
+        md="6"
+        lg="4"
+        class="navy--text font-weight-medium"
+      >
+        <div :id="location.name" @click="$emit('setLocation', location)">
+          <div
+            class="region-card px-2 py-2 d-flex align-center text--blue-dark font-weight-medium"
+            :id="location.name"
             :class="{
               active: isLocationActive(location),
               disable: isLocationDisabled(location),
-            }" @click="setLocation(location)">
-            <img class="mr-2" :src="location.image" :alt="location.name" :class="{
-              'filter-gray-60': location.disabled,
-            }" />
-            <div v-if="location.abbr === 'dtx'"
-              class="bookmark d-flex caption font-italic justify-center primary lighten-1 white--text">
+            }"
+            @click="setLocation(location)"
+          >
+            <img
+              class="mr-2"
+              :src="location.image"
+              :alt="location.name"
+              :class="{
+                'filter-gray-60': location.disabled,
+              }"
+            />
+            <div
+              v-if="location.abbr === 'dtx'"
+              class="bookmark d-flex caption font-italic justify-center primary lighten-1 white--text"
+            >
               10Gbps - NVMe SSD - 2.9+ GHz CPU
             </div>
 
             <div class="d-flex justify-space-between flex-grow-1 align-center">
               <p class="primary--text subtitle-2">{{ location.name }}</p>
               <span class="caption grey--text text--darken-1 add-price">
-                + ${{ parseFloat(location.monthlyPrice.toString()).toFixed(2) }} USD
+                + ${{ parseFloat(location.monthlyPrice.toString()).toFixed(2) }}
+                USD
               </span>
               <!-- <p v-if="isLocationDisabled(location)" class="caption">
               Coming Soon...
             </p> -->
             </div>
           </div>
-        </tag-manager-wrapper>
+        </div>
       </v-col>
     </v-row>
     <div class="caption silver--text">
@@ -44,31 +64,38 @@
 </template>
 
 <script setup lang="ts">
-import { Region } from '@/types/region'
-import { Location } from '@/models/location'
+import { Region } from "@/types/region";
+import { Location } from "@/models/location";
 
 interface PropsType {
-  locationList: Region[],
-  defaultLocation: Location
+  locationList: Region[];
+  defaultLocation: Location;
 }
-const selectedLocation = ref()
+const selectedLocationId = ref("");
 
 const props = defineProps<PropsType>();
-selectedLocation.value = props.defaultLocation.id
+selectedLocationId.value = props.defaultLocation.id;
 
-const emits = defineEmits(['setLocation']);
+const emits = defineEmits(["setLocation"]);
 
 const setLocation = (location: Location) => {
-  selectedLocation.value = location.id;
-  emits("setLocation", new Location(selectedLocation.value, location.configId, location.abbr.toUpperCase(), false));
-}
+  selectedLocationId.value = location.id;
+  emits(
+    "setLocation",
+    new Location(
+      selectedLocationId.value,
+      location.configId,
+      location.abbr.toUpperCase(),
+      false
+    )
+  );
+};
 const isLocationActive = (location: Location) => {
-  return selectedLocation.value === location.id;
-}
+  return selectedLocationId.value === location.id;
+};
 const isLocationDisabled = (location: Location) => {
   return location.disabled;
-}
-
+};
 </script>
 
 <style scoped lang="scss">
